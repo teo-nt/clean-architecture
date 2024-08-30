006bff7d02d415f9cb0182fd3d9a27a9874b610c
Theodoros Ntonas <thod_ntonas@hotmail.com>
2024-08-30 15:13:50 +0300

    Create new components

---
+++ client-app/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'semantic-ui-css/semantic.min.css'
+import './app/layout/styles.css'
+import App from './app/layout/App'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
