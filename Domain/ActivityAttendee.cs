69d1952ad46a1d23b5cab63005be32d0435f59b0
ntonast <ntonasT@unisystems.gr>
2024-10-15 17:42:52 +0300

    Configure relationships between Activities and Users

---
+++ Domain/ActivityAttendee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string AppUserId { get; set; } = string.Empty;
+        public AppUser AppUser { get; set; } = null!;
+        public Guid ActivityId { get; set; }
+        public Activity Activity { get; set; } = null!;
+
+        public bool IsHost { get; set; }
+    }
+}
