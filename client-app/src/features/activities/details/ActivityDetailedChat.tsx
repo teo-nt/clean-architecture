af89897f3f381698df0463af7b56ffd0ebe5a152
Teo <thod_ntonas@hotmail.com>
2024-12-27 15:50:27 +0200

    Add validation for comment submission

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,10 +1,11 @@
 import { observer } from 'mobx-react-lite'
-import { Segment, Header, Comment, Button } from 'semantic-ui-react'
+import { Segment, Header, Comment, Button, Loader } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Form, Formik } from 'formik';
+import { Field, FieldProps, Form, Formik } from 'formik';
 import MyTextArea from '../../../app/common/form/MyTextArea';
+import * as Yup from 'yup'
 
 interface Props {
     activityId: string;
@@ -44,28 +45,41 @@ export default observer(function ActivityDetailedChat({ activityId }: Props) {
                                 <Comment.Metadata>
                                     <div>{comment.createdAt}</div>
                                 </Comment.Metadata>
-                                <Comment.Text>{comment.body}</Comment.Text>                              
+                                <Comment.Text style={{whiteSpace: 'pre-wrap'}}>{comment.body}</Comment.Text>                              
                             </Comment.Content>
                         </Comment>
                     ))}
 
                     <Formik
                         onSubmit={(values, { resetForm }) => commentStore.addComment(values).then(() => resetForm())}
-                        initialValues={{body: ""}}
+                        initialValues={{ body: "" }}
+                        validationSchema={Yup.object({
+                            body: Yup.string().required()
+                        })}
                     >
-                        {({ isSubmitting, isValid }) => (
+                        {({ isSubmitting, isValid, handleSubmit }) => (
                             <Form className="ui form">
-                                <MyTextArea placeholder="Add comment" name='body' rows={2} />
-                                <Button
-                                    loading={isSubmitting}
-                                    disabled={isSubmitting || !isValid}
-                                    content='Add Reply'
-                                    labelPosition='left'
-                                    icon='edit'
-                                    primary
-                                    type='submit'
-                                    floated='right'
-                                />
+                                <Field name='body'>
+                                    {(props: FieldProps) => (
+                                        <div style={{ position: 'relative' }}>
+                                            <Loader active={isSubmitting} />
+                                            <textarea
+                                                placeholder='Enter your comment (Enter to submit, SHIFT + enter for new line)'
+                                                rows={2}
+                                                {...props.field}
+                                                onKeyDown={e => {
+                                                    if (e.key === 'Enter' && e.shiftKey) {
+                                                        return;
+                                                    }
+                                                    if (e.key === 'Enter' && !e.shiftKey) {
+                                                        e.preventDefault();
+                                                        isValid && handleSubmit()
+                                                    }
+                                                } }
+                                            />
+                                        </div>
+                                    ) }
+                                </Field>
                             </Form>
                         )}
                     </Formik>
