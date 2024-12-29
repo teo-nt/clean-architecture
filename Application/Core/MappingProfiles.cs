53e7dfb2f932e17c78a861e9d17671dc24c6eafe
Teo <thod_ntonas@hotmail.com>
2024-12-29 21:18:12 +0200

    Add controller for following and update profile class

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -19,7 +19,9 @@ namespace Application.Core
                 .ForMember(p => p.Bio, opt => opt.MapFrom(a => a.AppUser.Bio))
                 .ForMember(p => p.Image, opt => opt.MapFrom(a => a.AppUser.Photos.FirstOrDefault(x => x.IsMain)!.Url));
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d => d.Image, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+                .ForMember(d => d.Image, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url))
+                .ForMember(d => d.FollowersCount, opt => opt.MapFrom(s => s.Followers.Count))
+                .ForMember(d => d.FollowingCount, opt => opt.MapFrom(s => s.Followings.Count));
             CreateMap<Comment, CommentDto>()
                 .ForMember(p => p.DisplayName, opt => opt.MapFrom(a => a.Author!.DisplayName))
                 .ForMember(p => p.Username, opt => opt.MapFrom(a => a.Author!.UserName))
